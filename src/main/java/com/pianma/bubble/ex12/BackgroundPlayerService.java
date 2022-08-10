2506874fba6b6934d0d9811ad5cad3cbc4b9ab38
Pianma <ikkw2040@naver.com>
2022-08-11 02:48:46 +0900

    버그 수정

--- src/main/java/com/pianma/bubble/ex12/BackgroundPlayerService.java
+++ src/main/java/com/pianma/bubble/ex12/BackgroundPlayerService.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.awt.Color;
 
 //메인스레드 바쁨 -  키보드 이벤트를 처리하기 바쁨
 //백그라운드에서 계속 관찰
@@ -15,7 +16,7 @@ public class BackgroundPlayerService implements Runnable{
     public BackgroundPlayerService(Player player){
         this.player = player;
         try {
-            image = ImageIO.read(new File("image/backgroundMap.png"));
+            image = ImageIO.read(new File("image/backgroundMapService.png"));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -25,25 +26,36 @@ public class BackgroundPlayerService implements Runnable{
         while (true){
             //색상 확인
 
+            // 2. 벽 충돌 체크
+            // 색상 확인
             Color leftColor = new Color(image.getRGB(player.getX() - 10, player.getY() + 25));
-            Color rightColor = new Color(image.getRGB(player.getX() + 50 + 10, player.getY() + 25));
+            Color rightColor = new Color(image.getRGB(player.getX() + 50 + 15, player.getY() + 25));
+            // -2가 나온다는 뜻은 바닥에 색깔이 없이 흰색
+            int bottomColor = image.getRGB(player.getX() + 10, player.getY() + 50 + 5) // -1
+                    + image.getRGB(player.getX()+50 - 10, player.getY() + 50 + 5); // -1
 
-            int bottomColor = image.getRGB(player.getX() + 10, player.getY() + 50 + 5)
-                    + image.getRGB(player.getX() + 50 - 10, player.getY() + 50 + 5);
-
-            if (bottomColor != -2) {
+            // 바닥 충돌 확인
+            if(bottomColor != -2) {
+                //System.out.println("바텀 칼러 : "+bottomColor);
+                //System.out.println("바닥에 충돌함");
                 player.setDown(false);
-            } else if (!player.isUp() && !player.isDown()) {
-                player.down();
+            }else { // -2 일때 실행됨 => 내 바닥색깔이 하얀색이라는 것
+                if(!player.isUp() && !player.isDown()) {
+                    //System.out.println("다운 실행됨");
+                    player.down();
+                }
             }
 
-            if (leftColor.getRed() == 255 && leftColor.getBlue() == 0 && leftColor.getGreen() == 0) {
-                player.setLeft(false);
+            // 외벽 충돌 확인
+            if(leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
+                //System.out.println("왼쪽 벽에 충돌함");
                 player.setLeftWallCrash(true);
-            } else if (rightColor.getRed() == 255 && rightColor.getBlue() == 0 && rightColor.getGreen() == 0) {
-                player.setRight(false);
+                player.setLeft(false);
+            }else if(rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
+                //System.out.println("오른쪽 벽에 충돌함");
                 player.setRightWallCrash(true);
-            } else {
+                player.setRight(false);
+            }else {
                 player.setLeftWallCrash(false);
                 player.setRightWallCrash(false);
             }
@@ -51,7 +63,7 @@ public class BackgroundPlayerService implements Runnable{
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
 
